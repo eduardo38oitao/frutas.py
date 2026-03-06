ab00d5024adbba777daeba95690c9b181e927acf
eduardo38oitao <klen.soares@escola.pr.gov.br>
2026-03-06 11:46:36 -0300

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,12 @@
+import random as rd 
+
+print ("*************************")
+print ("Bem vindo a esccolha de frut")
+print ("*************************")
+
+frutas = ["Maçã", "Banana", "Caju", "Manga"]
+
+for indice, valor in enumerate(frutas):
+    print(f"Posição {indice}: {valor}")
+
+    
